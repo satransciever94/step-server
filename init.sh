f9b198a44a02ed3b0879b6764d91108d3c9bf5ed
Sergey Tabakaev <9997871+satransciever94@users.noreply.github.com>
2019-07-22 20:55:21 +0700

    Переписана команда запуска gunicorn

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-gunicorn hello:wsgi_application
+gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application &
 sudo /etc/init.d/nginx restart
