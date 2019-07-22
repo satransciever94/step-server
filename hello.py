6ceaa5976fe684a0528ee3c4166c251fb4366946
satransciever94 <9997871+satransciever94@users.noreply.github.com>
2019-07-22 07:39:08 +0700

    Конфигурация и логика приложения

---
+++ hello.py
@@ -0,0 +1,18 @@
+from cgi import parse_qs
+
+bind = "0.0.0.0:8080"
+
+def wsgi_application(environ, start_response):
+    # обработка строки запроса (query string)
+    zapros = parse_qs(environ['QUERY_STRING'])
+    otvetka = ''
+    for key in zapros:
+        for value in zapros[key]:
+            otvetka += key+'='+value+'\n'
+    otvetka = otvetka[:len(otvetka)-1]
+    # подготовка и передача ответа
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = otvetka
+    start_response(status, headers)
+    return [ body ]
