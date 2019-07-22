24e313ddc83af19ef493c24eb2971f6b882608eb
satransciever94 <9997871+satransciever94@users.noreply.github.com>
2019-07-22 08:01:57 +0700

    Оптимизация: убрана лишняя переменная

--- hello.py
+++ hello.py
@@ -13,6 +13,5 @@ def wsgi_application(environ, start_response):
     # подготовка и передача ответа
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    body = otvetka
     start_response(status, headers)
-    return [ body ]
+    return [ otvetka ]
