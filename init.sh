17f64caf6d3eadde462c3d26ddfd22de755d208e
satransciever94 <9997871+satransciever94@users.noreply.github.com>
2019-07-22 16:04:26 +0700

    Создание символической ссылки на программу и запуск Gunicorn

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+gunicorn hello:wsgi_application
 sudo /etc/init.d/nginx restart
